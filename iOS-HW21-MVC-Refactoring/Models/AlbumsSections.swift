33ff027cb4e26450d46904bd55a5bad0b3d7346a
Olga Mikhailova <foxx_fire@mail.ru>
2025-09-13 22:19:20 +0300

    Change AlbumsSections

---
+++ iOS-HW21-MVC-Refactoring/Models/AlbumsSections.swift
@@ -0,0 +1,27 @@
+//
+//  Sections.swift
+//  iOS-HW21-MVC-Refactoring
+//
+//  Created by FoxxFire on 13.09.2025.
+//
+
+import Foundation
+
+//MARK: - Section Type
+
+enum SectionType: String, CaseIterable {
+    case myAlbums = "My Albums"
+    case sharedAlbums = "Shared Albums"
+    case mediaTypes = "Media Types"
+    case other = "Other"
+}
+
+//MARK: - Section Model
+
+struct AlbumSection {
+    let header: SectionHeader  // Заголовок секции
+    let type: SectionType      // Тип секции (для layout)
+    let items: [CellItemProtocol]     // ✅ Массив ВСЕХ ячеек этой секции
+}
+
+
