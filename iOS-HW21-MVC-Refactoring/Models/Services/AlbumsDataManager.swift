bb4aeb44483c249c7be1a17345819f51c7da5d82
Olga Mikhailova <foxx_fire@mail.ru>
2025-09-13 22:20:03 +0300

    Change AlbumDataManager

--- iOS-HW21-MVC-Refactoring/Models/Services/AlbumsDataManager.swift
+++ iOS-HW21-MVC-Refactoring/Models/Services/AlbumsDataManager.swift
@@ -4,21 +4,34 @@
 //
 //  Created by FoxxFire on 12.09.2025.
 //
+import Foundation
 
 protocol AlbumsDataProtocol {
     func getSections() -> [AlbumSection]
     func getSection(at index: Int) -> AlbumSection?
+    func getSectionType(at index: Int) -> SectionType?
 }
 
 class AlbumsDataManager: AlbumsDataProtocol {
     
+    private let dataService: AlbumsDataServiceProtocol
+    
+    init(dataService: AlbumsDataServiceProtocol = AlbumsDataService()) {
+        self.dataService = dataService
+    }
+    
     func getSections() -> [AlbumSection] {
-        return AlbumSection.allSections
+        return dataService.getAllSections()
     }
     
     func getSection(at index: Int) -> AlbumSection? {
-        guard index < AlbumSection.allSections.count else { return nil }
-        return AlbumSection.allSections[index]
+        let sections = getSections()
+        guard index < sections.count else { return nil }
+        return sections[index]
+    }
+    
+    func getSectionType(at index: Int) -> SectionType? {
+        return getSection(at: index)?.type
     }
 }
 
